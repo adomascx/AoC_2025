fac148702baf5f6e64617ab41f8ab37625084010
adomascx <adlukosev2@gmail.com>
2026-01-14 00:53:36 +0200

    Set up TS environment, implement standardized input for TS implementation, update gitignores globally,

--- Day1/TS/script.js
+++
@@ -1,4 +0,0 @@
-(() => {
-  // script.ts
-  console.log("Hello World");
-})();
--- Day1/TS/script.ts
+++ Day1/TS/script.ts
@@ -1,12 +1,28 @@
-import fs from 'fs'
+import fs from "fs";
 
-const is = fs.createReadStream('../input.txt', 'utf-8');
+const is: string = fs.readFileSync("../input.txt", "utf-8").trim();
 
-console.log(is);
+// A single action/rotation of the dial. Corresponds to one line in the text file. contains:
+// direction (R/L)
+// degree (a uint)
+type action = {
+    direction: "R" | "L";
+    degree: number;
+}
 
-// let inputString = "R50";
+// Split input string into array of substrings for each line
+const allActionsString: string[] = is.split('\n');
 
-// if (inputString.startsWith('R')) {
-// }
+// Array of all actions that will be completed
+const allActions: action[] = [];
 
+allActionsString.forEach(thisActionString => {
+
+    let thisAction: action = {
+        direction: thisActionString.charAt(0) as "R" | "L",
+        degree: parseInt(thisActionString.slice(1))
+    };
+
+    allActions.push(thisAction);
+});
 
