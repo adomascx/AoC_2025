bf3dc4d107dd22db5b5a7d44cccb32e8e50625a4
adomascx <adlukosev2@gmail.com>
2026-01-18 20:37:14 +0200

    Hopefully finish migration to Deno, separate TS code into discrete functions, begin args functionality

--- Day1/TS/script.ts
+++ Day1/TS/script.ts
@@ -1,37 +1,35 @@
-import fs from "node:fs";
+const args = Deno.args;
 
-const is: string = fs.readFileSync("../input.txt", "utf-8").trim();
 
-// Split input string into array of substrings for each line
-const allActionsString: string[] = is.split('\n');
 
-// Array of all actions that will be completed
-const allActions: number[] = [];
+function processInput(path: string = "../input.txt") {
+  const inputString: string = Deno.readTextFileSync(path).trim();
 
-allActionsString.forEach(thisActionString => {
-    const thisAction = parseInt(thisActionString.slice(1)) * (thisActionString.charAt(0) === 'R' ? 1 : -1);
-    allActions.push(thisAction);
-});
+    // Array of all actions that will be completed
+    const allActions: number[] = [];
 
-// Track the current position of the dial (from 0 to 99; default is middle = 50)
-let currentPosition: number = 50;
+    inputString.split('\n').forEach(thisActionString => {
+        const thisAction = parseInt(thisActionString.slice(1)) * (thisActionString.charAt(0) === 'R' ? 1 : -1);
+        allActions.push(thisAction);
+    });
 
-// Counter for how many times the dial passed through 0
-let zeroCounter: number = 0;
+    return allActions;
+}
 
-allActions.forEach(thisAction => {
+function evalAction(action: number) {
     // Move the dial (direction depends on sign of thisAction)
-    currentPosition += thisAction;
+    currentPosition += action;
 
     // If the dial is left pointing at 0, increment zeroCounter
     if (currentPosition % 100 == 0) { zeroCounter++; }
 
     // God help whoever attempts to understand this
     // Count how many times dial passed *through* 0, add that many to zeroCounter
+
     // Conditionals for when dial is "overflowing, isn't currently at 0 and wasn't at 0 last action"
     const isOverflowing = currentPosition > 99 || currentPosition < 0;
     const isAtZeroNow = currentPosition % 100 === 0;
-    const wasAtZeroBeforeAction = (currentPosition - thisAction) === 0;
+    const wasAtZeroBeforeAction = (currentPosition - action) === 0;
 
     if (isOverflowing && !isAtZeroNow && !wasAtZeroBeforeAction) {
         // increment zeroCounter (C++ style int division)
@@ -42,6 +40,18 @@ allActions.forEach(thisAction => {
 
     // "fix" position back to range (0, 99)
     currentPosition = (currentPosition % 100 + 100) % 100;
+}
+
+const actions = processInput(inputFile);
+
+// Track the current position of the dial (from 0 to 99; default is middle = 50)
+let currentPosition: number = 50;
+
+// Counter for how many times the dial passed through 0
+let zeroCounter: number = 0;
+
+actions.forEach(thisAction => {
+    evalAction(thisAction);
 });
 
 console.log("The result is:", zeroCounter);
