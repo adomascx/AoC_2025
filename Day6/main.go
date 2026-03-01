a92c605ba7178723e387c45037d9051769615483
adomascx <adlukosev2@gmail.com>
2026-03-01 17:28:46 +0200

    Day6: complete task implementation, add small memory optimization

--- Day6/main.go
+++ Day6/main.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
 type Problem struct {
-	numbers   []int
+	numbers   []int64
 	operation Operation
 }
 
-type Operation int
+// since Go doesn't have enums, we create this
+// as a slight memory optimization, 'Operation' is typed as bool (1 byte) instead of int (8 bytes)
+type Operation bool
 
-// since Go doesn't have enums, we have to do this terribleness
 const (
-	multiplication Operation = iota
-	addition
+	multiplication Operation = false
+	addition       Operation = true
 )
+
+func parseOperation(inputToken string) (Operation, error) {
+	switch inputToken {
+
+	case "*":
+		return multiplication, nil
+
+	case "+":
+		return addition, nil
+
+	default:
+		return false, fmt.Errorf("could not parse operation %#v", inputToken)
+	}
+}
 
 func processInput(inputFile string) ([]Problem, error) {
 
@@ -35,23 +53,74 @@ func processInput(inputFile string) ([]Problem, error) {
 
 	// keep an index of which line of the file is currently being read
 	lineIdx := 0
-
 	for scanner.Scan() {
 
 		// read all space-separated values on this line
-		entries := strings.Fields(scanner.Text())
+		fields := strings.Fields(scanner.Text())
 
-		for i, v := range entries {
-			entryAsInt, _ := strconv.ParseInt(v)
-			problems[i].numbers[lineIdx] = append(problems[i].numbers[lineIdx], entryAsInt)
+		if lineIdx == 0 {
+			// on first run, pre-allocate memory so 0 index doesn't throw 'out of range'
+			problems = make([]Problem, len(fields))
 		}
+
+		// finds the last line indicated by the first symbol that is read, since the last line always contains 'operations'
+		if fields[0] == "*" || fields[0] == "+" {
+			// parse input fields
+			for i, field := range fields {
+				problems[i].operation, err = parseOperation(field)
+				if err != nil {
+					return nil, fmt.Errorf("could not do parseOperation(%#v):  %w", field, err)
+				}
+			}
+
+			return problems, nil
+		}
+
+		// place each field into its respective problem,
+		// so each problem's numbers are built up incrementally as lines are read
+		for i, v := range fields {
+			// parse input fields
+			entryAsInt, err := strconv.ParseInt(v, 0, 0)
+			if err != nil {
+				return nil, fmt.Errorf("could not do ParseInt(%v):  %w", v, err)
+			}
+
+			problems[i].numbers = append(problems[i].numbers, entryAsInt)
+		}
+
 		lineIdx++
 	}
 
-	return nil, nil
+	return nil, errors.New("processInput did not terminate at 'operations'")
 }
 
 func main() {
 
-	processInput("input.test.txt")
+	problems, err := processInput("input.txt")
+	if err != nil {
+		log.Fatalf("could not do processInput():  %v", err)
+	}
+
+	var problemSum int64
+
+	for _, problem := range problems {
+		switch problem.operation {
+
+		case addition:
+			for _, number := range problem.numbers {
+				problemSum += number
+			}
+
+		case multiplication:
+			var currAnswer int64 = 1
+
+			for _, number := range problem.numbers {
+				currAnswer *= number
+			}
+
+			problemSum += currAnswer
+		}
+	}
+
+	fmt.Printf("The answer is: %v", problemSum)
 }
